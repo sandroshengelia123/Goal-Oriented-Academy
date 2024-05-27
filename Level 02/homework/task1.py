2a6f3607aa84630dde00e3c9d0437bc0d2b7aa97
sandroshengelia123 <sandroshengo@yahoo.com>
2024-05-27 22:21:23 +0400

    commit

---
+++ Level 02/homework/task1.py
@@ -0,0 +1,28 @@
+name = 'sandro'
+last_name = 'shengelia'
+proffesion = 'web develooper'
+age = 16 
+country = 'georgia'
+print(name + last_name + proffesion + age + country )
+
+
+
+print(10 + 20)
+print(40 - 20)
+print(5 * 6)
+print(10 // 5)
+print(10 ** 2)
+print(40 +50)
+print(50 - 20)
+print(6 * 7 )
+print(10 // 2 )
+print(10 ** 3)
+
+
+
+
+print(10 + 20 + 40 - 20 - 5 * 6 - 10 // 5 - 10 ** 2 - 40 + 50 - 50 - 20 - 6 * 7 - 10 // 2 - 10 ** 3 )
+
+
+
+
