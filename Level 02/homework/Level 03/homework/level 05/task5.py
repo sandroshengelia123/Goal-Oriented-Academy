12e15e35cb6a51b07c5c1cca332f31f66f007f05
sandroshengelia123 <sandroshengo@yahoo.com>
2024-07-26 22:13:03 +0400

    level

---
+++ Level 02/homework/Level 03/homework/level 05/task5.py
@@ -0,0 +1,57 @@
+print(10 + 20)
+print(20 - 10)
+print(320 ** 320)
+print(5 // 5)
+
+mother  = 47 + 20 = 67
+father = 42 + 20 = 62
+grandfather = 70 + 20 = 90
+grandpa = 77 + 20 = 92
+sister = 12 + 20 = 32
+me = 16 + 20 = 36
+
+name = sandro
+last_name = shengelia
+age = 16
+country = georgia
+city = tbilisi
+favorite color = blue 
+favorite car = lamborghini
+favorite food = khinkali
+favorite sport = basketball
+
+print(10 + 20)
+print(20 - 10)
+print(320 ** 320)
+print(5 // 5)
+
+mother  = 47 + 20 = 67
+father = 42 + 20 = 62
+grandfather = 70 + 20 = 90
+grandpa = 77 + 20 = 92
+sister = 12 + 20 = 32
+me = 16 + 20 = 36
+
+name = sandro
+last_name = shengelia
+age = 16
+country = georgia
+city = tbilisi
+favorite color = blue 
+favorite car = lamborghini
+favorite food = elarj
+favorite sport = basketball
+
+
+
+hi my name is sandro I am 16 years old i live in my country my country name is georgia city of georgia is 
+Tbilisi. my favorite color is blue also green my favorite car is lamborghini also my favorite food is elarji 
+because I am megreli my favorite sport is basketball and also boxing
+
+
+
+
+
+
+
+
