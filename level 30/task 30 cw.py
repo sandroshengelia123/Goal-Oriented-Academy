adee48900521b8cabf2672b2211283dda4b44faa
sandroshengelia123 <sandroshengo@yahoo.com>
2024-10-16 21:28:52 +0400

    comment

---
+++ level 30/task 30 cw.py
@@ -0,0 +1,27 @@
+# my_list = [1,2,3,4,5]
+
+# name = input("Enter Name: ")
+
+# my_List.appened(name)
+
+# print(my_list)
+
+# append - დამატება
+# pop - წაშლა
+# remove - წაშლა
+
+
+
+# შექმენით მენტორების სახელების სია
+
+
+
+
+my_list =["gabrieli, daviti, giorgi, nika,"]
+
+
+print(my_list)
+
+my_list.remove("giorgi")
+
+print(my_list)
